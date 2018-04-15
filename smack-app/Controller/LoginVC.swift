c7fed7e5da3da3381bb3e1d328d647f24c1fd665
Dan Sims <sims.daniel.ds@gmail.com>
2018-04-15 13:29:45 -0400

    end of lesson 4.

---
+++ smack-app/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  smack-app
+//
+//  Created by Dan Sims on 4/15/18.
+//  Copyright © 2018 Dan Sims. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ smack-app/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack-app
+//
+//  Created by Dan Sims on 4/15/18.
+//  Copyright © 2018 Dan Sims. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
