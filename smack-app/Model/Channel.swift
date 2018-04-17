d8a7c6e153fdee4ff1e0ad1bbb88822c64323ebc
Dan Sims <sims.daniel.ds@gmail.com>
2018-04-17 10:36:29 -0400

    lesson 19 finished

---
+++ smack-app/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  smack-app
+//
+//  Created by Dan Sims on 4/17/18.
+//  Copyright © 2018 Dan Sims. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+}
